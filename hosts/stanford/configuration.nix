0a6766c1cb1aac4bc175a4320c92c16163dba01a
Eric Park <me@ericswpark.com>
2024-10-24 11:59:20 -0400

    stanford: add Bluetooth

--- hosts/stanford/configuration.nix
+++ hosts/stanford/configuration.nix
@@ -34,6 +34,12 @@
     nvidiaBusId = "PCI:1:0:0";
   };
 
+  # Add Bluetooth support
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
