1adca461960c26d1a31e0ebde9e3ebcfd0a7c9eb
Eric Park <me@ericswpark.com>
2024-11-19 15:15:47 -0500

    Use nm-online to check for network connectivity
    network-online is a system systemd service, and thus cannot be
    referenced in user systemd services. Use `nm-online` which blocks until
    NetworkManager can establish a connection as a workaround.

--- home-manager/common/rclone-purdue.nix
+++ home-manager/common/rclone-purdue.nix
@@ -4,8 +4,6 @@
     systemd.user.services.rclone-purdue-mount = {
         Unit = {
             Description = "Mount Purdue OneDrive with rclone";
-            After = [ "network-online.target" ];
-            Requires = [ "network-online.target" ];
         };
         Install = {
             WantedBy = [ "default.target" ];
@@ -19,7 +17,10 @@
         in
             {
                 Type = "simple";
-                ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${mountDir}";
+                ExecStartPre = [
+                    "/run/current-system/sw/bin/nm-online -q"
+                    "/run/current-system/sw/bin/mkdir -p ${mountDir}"
+                ];
                 ExecStart = "${pkgs.rclone}/bin/rclone mount --vfs-cache-mode full ${serviceConfigName} ${mountDir}";
                 ExecStop = "/run/current-system/sw/bin/fusermount -u ${mountDir}";
                 Restart = "on-failure";
