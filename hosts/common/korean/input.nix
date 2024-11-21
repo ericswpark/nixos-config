a50e299bc9c80ba95276d872bd21e0205e4e9623
Eric Park <me@ericswpark.com>
2024-11-21 16:30:34 -0500

    Workaround kime bug with fcitx5 for now
    Workaround for #1

--- hosts/common/korean/input.nix
+++ hosts/common/korean/input.nix
@@ -1,11 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
     i18n = {
         defaultLocale = "en_US.UTF-8";
         inputMethod = {
             enable = true;
-            type = "kime";
-            kime.iconColor = "White";
+            type = "fcitx5";
+            fcitx5 = {
+                addons = with pkgs; [
+                    kdePackages.fcitx5-qt
+                    fcitx5-hangul
+                ];
+                waylandFrontend = true;
+            };
         };
     };
 }
