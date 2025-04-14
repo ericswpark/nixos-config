b0ecbc1e01e2bb2d8565b28753355c2824841146
Eric Park <me@ericswpark.com>
2025-04-14 15:57:21 -0400

    wendy: home-manager: restructure

--- hosts/wendy/home-manager.nix
+++ hosts/wendy/home-manager.nix
@@ -1,69 +1,84 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "ericswpark";
-  home.homeDirectory = "/home/ericswpark";
 
   imports = [
     ../../home-manager/common
     ../../home-manager/plasma-manager
   ];
 
-  home.packages = with pkgs; [
-    neofetch
-    discord
-    vscode
-    obsidian
-    bitwarden
-    slack
-    telegram-desktop
-    thunderbird-bin
-    mosh
-    mpv
-    yt-dlp
-    btop
-    rclone
-    gnupg
-    pinentry-tty
-    element-desktop
-    kuro
-    moonlight-qt
-    jellyfin-media-player
-    anki-bin
-    wine
-    bottles
-    tor-browser
-    signal-desktop
-    neovim
-    unzip
-    python3
-    kdePackages.kdenlive
-    arduino-ide
-    screen
-    minicom
-    prusa-slicer
-    android-tools
-    trayscale
-    google-chrome
-    kdePackages.partitionmanager
-    gparted
-    freecad
-    gnumake
-    gcc
-    rustup
-    jetbrains.rust-rover
-    jetbrains.idea-ultimate
-    jetbrains.pycharm-professional
-    android-studio
-    nixfmt-rfc-style
-    nil
-  ];
-
-  home.stateVersion = "24.05";
-
   programs.home-manager.enable = true;
 
-  home.sessionVariables.EDITOR = "nvim";
+  home = {
+    username = "ericswpark";
+    homeDirectory = "/home/ericswpark";
+
+    packages = with pkgs; [
+      neofetch
+      discord
+      vscode
+      obsidian
+      bitwarden
+      slack
+      telegram-desktop
+      thunderbird-bin
+      mosh
+      mpv
+      yt-dlp
+      btop
+      rclone
+      gnupg
+      pinentry-tty
+      element-desktop
+      kuro
+      moonlight-qt
+      jellyfin-media-player
+      anki-bin
+      wine
+      bottles
+      tor-browser
+      signal-desktop
+      neovim
+      unzip
+      python3
+      kdePackages.kdenlive
+      arduino-ide
+      screen
+      minicom
+      prusa-slicer
+      android-tools
+      trayscale
+      google-chrome
+      kdePackages.partitionmanager
+      gparted
+      freecad
+      gnumake
+      gcc
+      rustup
+      jetbrains.rust-rover
+      jetbrains.idea-ultimate
+      jetbrains.pycharm-professional
+      android-studio
+      nixfmt-rfc-style
+      nil
+    ];
+
+    sessionVariables.EDITOR = "nvim";
+
+    # Use my dotfiles repository
+    # git clone git@github.com:ericswpark/dot_files.git ~/.dotfiles
+    file = {
+      ".config/mpv" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/mpv/.config/mpv";
+      };
+
+      ".vimrc" = {
+        source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vim/.vimrc";
+      };
+    };
+
+    stateVersion = "24.05";
+  };
 
   programs.bash = {
     enable = true;
@@ -72,17 +87,5 @@
       # include .profile if it exists
       [[ -f ~/.profile ]] && . ~/.profile
     '';
-  };
-
-  # Use my dotfiles repository
-  # git clone git@github.com:ericswpark/dot_files.git ~/.dotfiles
-  home.file = {
-    ".config/mpv" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/mpv/.config/mpv";
-    };
-
-    ".vimrc" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/vim/.vimrc";
-    };
   };
 }
