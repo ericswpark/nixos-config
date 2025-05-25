a1d2b4be512620f83329985c377c8d2831600cf0
Eric Park <me@ericswpark.com>
2025-05-25 13:32:51 -0400

    hw: bluetooth: add BlueZ settings options

--- hosts/common/hw/bluetooth.nix
+++ hosts/common/hw/bluetooth.nix
@@ -3,5 +3,11 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
+    settings.General = {
+      Privacy = "device";
+      JustWorksRepairing = "always";
+      Class = "0x000100";
+      FastConnectable = true;
+    };
   };
 }
