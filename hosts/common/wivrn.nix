3713bda31437daf373bd4cf99e82db09909bb7ee
Eric Park <me@ericswpark.com>
2025-06-04 19:37:35 -0400

    Make WiVRn take precedence over Monado if imported together

--- hosts/common/wivrn.nix
+++ hosts/common/wivrn.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   services.wivrn = {
     enable = true;
@@ -24,4 +24,7 @@
       };
     };
   };
+
+  # Force WiVRn to take priority if imported with Monado
+  environment.etc."xdg/openxr/1/active_runtime.json".source = lib.mkForce "${pkgs.wivrn}/share/openxr/1/openxr_wivrn.json";
 }
