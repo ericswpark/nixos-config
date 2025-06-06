7eabe1216d1fe08f04648c0a7d476593aa25e4c3
Eric Park <me@ericswpark.com>
2025-06-05 23:10:27 -0400

    hosts: common: wivrn: remove config
    The defaults provided by WiVRn are sane, and they get overwritten anyway
    by the client if the user makes any changes.

--- hosts/common/wivrn.nix
+++ hosts/common/wivrn.nix
@@ -5,24 +5,6 @@
     openFirewall = true;
     defaultRuntime = true;
     autoStart = true;
-
-    config = {
-      enable = true;
-      json = {
-        scale = 1.0;
-        bitrate = 100000000;
-        encoders = [
-          {
-            encoder = "vaapi";
-            codec = "h265";
-            width = 1.0;
-            height = 1.0;
-            offset_x = 0.0;
-            offset_y = 0.0;
-          }
-        ];
-      };
-    };
   };
 
   # Force WiVRn to take priority if imported with Monado
