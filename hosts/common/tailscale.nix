fc5492d2e05d3f4157a752aa4a1c261921fa4174
Eric Park <me@ericswpark.com>
2025-06-08 13:13:29 -0400

    tailscale: use systemd-resolved for DNS
    See: https://github.com/tailscale/tailscale/issues/4254
    
    If this option is not enabled, then I lose DNS resolution every time my
    laptop goes to sleep and wakes up, which is incredibly annoying. I
    thought it was my Ethernet adapter going bad until I realized I could
    ping hosts by IP but not dig for hostnames.

--- hosts/common/tailscale.nix
+++ hosts/common/tailscale.nix
@@ -4,4 +4,8 @@
     enable = true;
     useRoutingFeatures = "both";
   };
+
+  # Use systemd-resolved
+  # See https://github.com/tailscale/tailscale/issues/4254
+  services.resolved.enable = true;
 }
