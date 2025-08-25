fcf289874a6d5ba4382f78c6af2b3baffd7ebf35
Eric Park <me@ericswpark.com>
2025-08-25 16:16:31 -0400

    fix(syncthing): don't create default Sync folder

--- hosts/common/syncthing.nix
+++ hosts/common/syncthing.nix
@@ -5,5 +5,6 @@
     openDefaultPorts = true;
     user = "ericswpark";
     dataDir = "/home/ericswpark/Syncthing";
+    extraFlags = [ "--no-default-folder" ]; # Don't create default ~/Sync folder
   };
 }
