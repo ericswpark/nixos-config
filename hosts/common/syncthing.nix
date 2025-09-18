c8c5a9ce0413e17254c7966a76916fe06fb4ae42
Eric Park <me@ericswpark.com>
2025-09-18 13:20:54 -0400

    fix: remove --no-default-folder flag for Syncthing
    This is not an option on Syncthing v2

--- hosts/common/syncthing.nix
+++ hosts/common/syncthing.nix
@@ -5,6 +5,5 @@
     openDefaultPorts = true;
     user = "ericswpark";
     dataDir = "/home/ericswpark/Syncthing";
-    extraFlags = [ "--no-default-folder" ]; # Don't create default ~/Sync folder
   };
 }
